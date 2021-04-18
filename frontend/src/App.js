b55f284b1f205f18990cf5a8bc82a01a365e0dba
rohit gupta <rohitgupta.che18@itbhu.ac.in>
2021-04-18 21:40:58 +0530

    lazy loading added in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import Users from './user/pages/Users'
-import NewPlace from './places/pages/NewPlace'
+// import Users from './user/pages/Users'
+// import NewPlace from './places/pages/NewPlace'
 import Navbar from './Components/Navigation/Navbar'
-import UserPlaces from './places/pages/UserPlaces'
+// import UserPlaces from './places/pages/UserPlaces'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import UpdatePlace from './places/pages/UpdatePlace'
-import Auth from './user/pages/Auth'
-import SignUp from './user/pages/SignUp'
+// import UpdatePlace from './places/pages/UpdatePlace'
+// import Auth from './user/pages/Auth'
+// import SignUp from './user/pages/SignUp'
 import { AuthContext } from './context/authContext'
 import { useAuth } from './Components/Hooks/auth-hook'
+import Loader from './Components/UIElements/Loader'
+
+const Users = React.lazy(() => import('./user/pages/Users'))
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'))
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'))
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'))
+const Auth = React.lazy(() => import('./user/pages/Auth'))
+const SignUp = React.lazy(() => import('./user/pages/SignUp'))
 
 const App = () => {
 
@@ -26,25 +34,31 @@ const App = () => {
     }}>
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/auth" exact component={Auth} />
-          <Route path="/auth/signup" exact>
-            <SignUp />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId" >
-            <UpdatePlace />
-          </Route>
-          <Route path="/:userId/places">
-            <UserPlaces />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
+        <Suspense fallback={
+          <div className="center">
+            <Loader />
+          </div>
+        }>
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/auth" exact component={Auth} />
+            <Route path="/auth/signup" exact>
+              <SignUp />
+            </Route>
+            <Route path="/places/new" exact>
+              <NewPlace />
+            </Route>
+            <Route path="/places/:placeId" >
+              <UpdatePlace />
+            </Route>
+            <Route path="/:userId/places">
+              <UserPlaces />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </Suspense>
       </Router>
     </AuthContext.Provider>
   );
